0b4a9461cc7da78f3bfda0bd6b29e91e95fe8f84
Flo <florian.unternaehrer@simplificator.com>
2017-10-27 21:36:56 +0200

    add a simple firebase based login approach

--- server/server.js
+++ server/server.js
@@ -1,15 +1,45 @@
 const express = require("express")
+const firebase = require("firebase")
+const bodyParser = require("body-parser")
 
 const app = express();
 const port = 8000;
 
+const config = {
+  apiKey: "AIzaSyAj9lpubh4xJl8Cdg0kyM862gIRM5p3xG0",
+  authDomain: "smallgardens-88a3e.firebaseapp.com",
+  databaseURL: "https://smallgardens-88a3e.firebaseio.com",
+  projectId: "smallgardens-88a3e",
+  storageBucket: "smallgardens-88a3e.appspot.com",
+  messagingSenderId: "157738393736"
+};
 
-app.get("/login", (req, res) => {
-  if (req.query.username == "simply" && req.query.password == "simply") {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(403);
-  }
+
+
+firebase.initializeApp(config);
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.post("/login", (req, res) => {
+  email = req.body.username
+  password = req.body.password
+
+  console.log(email)
+  console.log(password)
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log("foooobar");
+      res.sendStatus(200)
+    })
+    .catch((error) => {
+      res.sendStatus(403)
+    });
 });
 
 
